6fa92092119d951575622da78ca8c85908abe29c
James Laverack <neoncode@gmail.com>
2013-02-11 03:47:21 +0000

    Adds Vector3D struct

---
+++ Vector3D.h
@@ -0,0 +1,21 @@
+/*
+ * Vector3D.h
+ *
+ *  Created on: 11 Feb 2013
+ *      Author: james
+ */
+
+#ifndef VECTOR3D_H_
+#define VECTOR3D_H_
+
+namespace photonCPU {
+
+typedef struct vec_tag {
+    float x;
+    float y;
+    float z;
+} Vector3D;
+
+}
+
+#endif /* VECTOR3D_H_ */
--- photon-tracer-cpu.cpp
+++ photon-tracer-cpu.cpp
@@ -8,8 +8,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "Vector3D.h"
+using photonCPU::Vector3D;
+
 
 int main(void) {
+
+	Vector3D vec;
+
+	vec.x = 100.0f;
+	printf("(%f, %f, %f)\n", vec.x, vec.y, vec.z);
+
+
 	puts("!!!Hello World!!!");
 	return EXIT_SUCCESS;
 }
