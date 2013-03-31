67d1f0ec5adaf2d203f58ccb6d39bf304c116c6f
James Laverack <jl9804@bris.ac.uk>
2013-03-31 15:07:00 +0100

    Move colour modifier into a single variable I am never(tm) going to want to have different ones for different colours.

--- src/CameraMaterial.cpp
+++ src/CameraMaterial.cpp
@@ -70,9 +70,10 @@ Ray* CameraMaterial::transmitRay(Vector3D* hitLocation, Vector3D* angle, Vector3
 
 			//printset = false;
 			//printf("wavelength %f to colour set %f, %f, %f\n", wavelength, r, g, b);
-			img->imageR[img->index(tu, tv)] += r*0.01;
-			img->imageG[img->index(tu, tv)] += g*0.01;
-			img->imageB[img->index(tu, tv)] += b*0.01;
+			float modifier = 0.005;
+			img->imageR[img->index(tu, tv)] += r*modifier;
+			img->imageG[img->index(tu, tv)] += g*modifier;
+			img->imageB[img->index(tu, tv)] += b*modifier;
 			// Ray is finished
 			return 0;
 		}
